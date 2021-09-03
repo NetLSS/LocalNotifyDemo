7fc75542b78926513a590e6a261bcc5d703be498
NetLSS <futureprogrammer@naver.com>
2021-09-03 15:27:17 +0900

    Feat: 알림 채널 생성 및 설정

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LocalNotifyDemo
 Android Local Notify Demo project.
+
+본 페이지는 [안드로이드 알림 개요](https://github.com/NetLSS/AndroidStudy/blob/3f3884a410da293ed157d4accda52588fe35e72e/post/Android/%ED%95%B5%EC%8B%AC%EB%A7%8C_%EA%B3%A8%EB%9D%BC%EB%B0%B0%EC%9A%B0%EB%8A%94_android_arcticFox/70.%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C_%EC%95%8C%EB%A6%BC_%EA%B0%9C%EC%9A%94.md)와 연결됩니다.
+
+[screenshot](https://i.imgur.com/JXVVQna.png)
--- app/src/main/java/com/lilcode/example/localnotifydemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/localnotifydemo/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.lilcode.example.localnotifydemo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.ClipDescription
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
+    private var notificationManager: NotificationManager? = null
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel(
+            "com.lilcode.example.localnotifydemo",
+            "Notify News",
+            "Example news Channel"
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(id: String, name: String, description: String) {
+        val importance = NotificationManager.IMPORTANCE_LOW
+        // 채널 인스턴스 생성 및 초기화
+        val channel = NotificationChannel(id, name, importance).apply {
+            this.description = description
+            enableLights(true)
+            lightColor = Color.RED
+            enableVibration(true)
+            vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+        }
+
+        notificationManager?.createNotificationChannel(channel)
+
     }
 }
