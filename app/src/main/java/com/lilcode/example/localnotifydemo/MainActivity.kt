ebee1a1c663302950cdae4a9f6e790296e7f9aff
NetLSS <futureprogrammer@naver.com>
2021-09-03 15:58:02 +0900

    Feat: 알림으로부터 액티비티 실행 시키기

--- app/src/main/java/com/lilcode/example/localnotifydemo/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/localnotifydemo/MainActivity.kt
@@ -3,8 +3,10 @@ package com.lilcode.example.localnotifydemo
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ClipDescription
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -43,11 +45,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         notificationManager?.createNotificationChannel(channel)
-
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun sendNotification(view: View) {
+
+        val resultIntent = Intent(this, ResultActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        // PendingIntent.FLAG_UPDATE_CURRENT : 팬딩 인텐트가 이미 존재할경우, Extra data를 새로운 인텐트 데이터로 업데이트
+        // 각각의 알림에서는 팬딩인텐트가 동일 하도록
+
         // 알림 생성
         val notification = Notification.Builder(this@MainActivity, newsChannelID)
             .setContentTitle("Example Notification")
@@ -55,6 +63,7 @@ class MainActivity : AppCompatActivity() {
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setChannelId(newsChannelID)
             .setNumber(999)
+            .setContentIntent(pendingIntent)
             .build()
 
         val notificationID = 101 // 정수값 아무거나 (향후 알림 변경시 사용됨)
